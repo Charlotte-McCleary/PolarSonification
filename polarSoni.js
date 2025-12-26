95483a7e1285bf6a0c0e3953cc531fe7857ae130
nitya sharma <nsharma1@students.chccs.k12.nc.us>
2025-12-26 15:34:44 -0500

    Added sonify(radius) function

--- polarSoni.js
+++ polarSoni.js
@@ -1,1 +1,23 @@
+import nj from 'numjs';
+//import sounddevice. i'm not sure what package javascript has which is similar to sounddevice
+
+function sonify(radius){
+  //Sonify the curve given in polar form by 'r = radius(theta)'. 
+  //:param radius: the radius function. 'radius' should take and return numpy arrays
+
+  const duration = 4.0;
+  const sampleRate = 44100;
+  const sampleCount = Math.floor(sampleRate * duration);
+  const theta = linspace(0, 2 * Math.PI, sampleCount);
+  const r = radiusFn(theta);
+  const dtheta = theta[1] - theta[0];
+  const phase = cumsum(r).map(v => 400 * 2 * Math.PI * v * dtheta);
+  //const tone
+  for (let i = 0; i < sampleCount; i++){
+    if (radiusFn([theta[i]])[0] < 0){
+      tone[i] += (Math.random() * 0.02 - 0.01);
+    }
+  }
+  //sd.play(tone, samplerate = sampleRate)
+  
 
